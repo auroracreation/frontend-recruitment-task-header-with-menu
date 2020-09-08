bfd635648be6c1ad9de447d6e5a7cbd7e66d23eb
heorhi-bel <georgix@mail.ru>
2020-09-08 17:13:18 +0200

    koszykPanel technical is done

--- script's/koszyk.js
+++ script's/koszyk.js
@@ -1,8 +1,28 @@
 "use strict"
 
+let koszykElem = header.querySelector('.header__bar__container-koszyk')
+
 function koszykOpening(event){
+    if(koszykElem.classList.contains('koszyk-is-open') && !event.target.classList.contains('header__bar__container-koszyk')){
+        console.log('step 1')
+        koszykElem.classList.remove('koszyk-is-open');
+        console.log('step 2')
+        koszykPanel.remove();
+        console.log('step 3')
+    }
     if(!event.target.classList.contains('header__bar__container-koszyk')) return;
-    
+    if(!koszykElem.classList.contains('koszyk-is-open')){
+        koszykElem.classList.add('koszyk-is-open');
+        koszykElem.insertAdjacentHTML('beforeend', 
+        `<div id='koszykPanel'>
+            <img src='' />
+            <h3>seven</h3>
+            <p>one two</h3>
+            <p>$30</h3>
+        </div>`
+        )
+        return;
+    }
 }
 
 header.addEventListener('click', koszykOpening)
