345d3ea1b3ca6aba1b25b54a250fcc25773ff985
Szymon Ludwikowski <ludwikowski.szym1993@gmail.com>
2023-06-26 16:05:54 +0200

    feat(index) basic structure added

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+
+gulp.task('styles', () => {
+  return gulp.src('scss/main.scss').pipe(sass()).pipe(gulp.dest('./styles/'))
+})
+
+gulp.task('watch', () => {
+  gulp.watch('scss/**/*.scss', (done) => {
+    gulp.series(['styles'])(done)
+  })
+})
